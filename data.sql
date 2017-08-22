f24cc4f197e2383af944c6426058c89d6e64cd26
Drew Bragg <DRBragg@gmail.com>
2017-08-22 07:49:28 -0400

    step 2

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO movies (title, year, catagory) VALUES ("Young Frankenstein", 1974, "Comedy");
+INSERT INTO movies (title, year, catagory) VALUES ("Blade Runner", 1982, "Sci-Fi");
+INSERT INTO movies (title, year, catagory) VALUES ("Rouge One", 2016, "Sci-Fi");
