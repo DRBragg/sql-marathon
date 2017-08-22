17e5b630d94171bc2fb54b09f0530a223fe2ff52
Drew Bragg <DRBragg@gmail.com>
2017-08-22 08:10:00 -0400

    step 4

--- schema.sql
+++ schema.sql
@@ -1,6 +1,13 @@
+DROP TABLE movies CASCADE;
+
 CREATE TABLE movies(
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   year INT,
-  catagory VARCHAR
+  category_id INT REFERENCES categories(id)
+);
+
+CREATE TABLE catagories(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
 );
